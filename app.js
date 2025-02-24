6a8b93fc0137f202f062739c5903e4a2c3075d3c
DieguitoNC <diegocatalann@gmail.com>
2025-02-24 10:19:34 -0300

    tratando cors

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: false})) //apenas dados simples
 app.use(bodyParser.json()) //Aceita apenas formato JSON de entrada no body
 
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin','*' ) //dando permissao para todos, caso fosse um servidor especifico, colocar o link dele nesse *
+    res.header(
+        'Access-Control-Allow-Header', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    ); // O que aceita de cabecalho (propriedades)
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+        return res.status(200).send({})
+    }
+
+    next();
+})
+
 app.use("/products", routeProducts);
 app.use("/order", routeOrder);
 
